87ee6b426fa8edff04271fcb93789d103456d255
Jinhyuk Kim <reniowood@gmail.com>
2017-06-12 04:20:08 +0900

    [client] react-native 설정

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Login from './app/components/Login/Login';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
