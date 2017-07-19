06a82cba7b76e9d43df114a50fd6115d54d3ab4a
Jinhyuk Kim <reniowood@gmail.com>
2017-07-19 20:17:54 +0900

    [client] keyboardSpacer 제거

--- client/App.js
+++ client/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import KeyboardSpacer from 'react-native-keyboard-spacer';
 import { ScreenNavigator } from './app/components/ScreenNavigator';
 
 export default class App extends React.Component {
@@ -8,7 +7,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <ScreenNavigator style={styles.screenNavigator} />
-        <KeyboardSpacer />
       </View>
     );
   }
