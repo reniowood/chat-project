8f2d2eab22d40d597af92ac26ba656e22b9a1a01
Jinhyuk Kim <reniowood@gmail.com>
2017-08-29 15:25:16 +0900

    [client] simple splash added

--- client/android/app/src/main/java/com/client/MainActivity.java
+++ client/android/app/src/main/java/com/client/MainActivity.java
@@ -1,6 +1,10 @@
 package com.client;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
@@ -10,5 +14,17 @@ public class MainActivity extends SplashActivity {
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
+    }
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout layout = new LinearLayout(this);
+
+        layout.setBackgroundColor(Color.parseColor("#f1f3ce"));
+        layout.setGravity(Gravity.CENTER);
+
+
+
+        return layout;
     }
 }
