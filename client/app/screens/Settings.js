dfa0baed1671fcf82e8a7b430f378e6949a0948d
Jinhyuk Kim <reniowood@gmail.com>
2017-08-30 02:27:59 +0900

    [client] logout implemented

--- client/app/screens/Settings.js
+++ client/app/screens/Settings.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { View, SectionList, StyleSheet } from 'react-native';
+import { View, SectionList, Alert, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 import Screen from './Screen';
+import { initUser } from '../actions/user';
+import { initChats } from '../actions/chats';
+import { initContacts } from '../actions/contacts';
 import TwoLineListItem from '../components/TwoLineListItem';
 
 class Settings extends Screen {
+    logOut() {
+        const { navigator, initUser, initChats, initContacts } = this.props;
+
+        initUser();
+        initChats();
+        initContacts();
+
+        navigator.resetTo({
+            screen: 'com.client.Login',
+        });
+    }
+
+    onPressLogoutListItem() {
+        Alert.alert(
+            '로그아웃',
+            '정말 로그아웃 하시겠습니까?',
+            [
+                { text: '아니오' },
+                { text: '예', onPress: this.logOut.bind(this) }
+            ]
+        );
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -17,7 +44,8 @@ class Settings extends Screen {
                             }, {
                                 key: 2,
                                 title: '로그아웃',
-                                subtitle: '현재 로그인한 계정을 로그아웃합니다.'
+                                subtitle: '현재 로그인한 계정을 로그아웃합니다.',
+                                onPress: this.onPressLogoutListItem.bind(this),
                             }],
                             title: "계정",
                             renderItem: ({item}) => {
@@ -25,6 +53,7 @@ class Settings extends Screen {
                                     <TwoLineListItem
                                         title={item.title}
                                         subtitle={item.subtitle}
+                                        onPress={item.onPress}
                                     />
                                 );
                             }
@@ -42,4 +71,24 @@ const styles = StyleSheet.create({
     },
 });
 
-export default Settings;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        user: state.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        initUser: () => {
+            dispatch(initUser());
+        },
+        initChats: () => {
+            dispatch(initChats());
+        },
+        initContacts: () => {
+            dispatch(initContacts());
+        },
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
