517ead8926577064ee1fb438d790dc71dcda34a5
Jinhyuk Kim <reniowood@gmail.com>
2017-08-29 22:50:38 +0900

    [client] section list added to Settings

--- client/app/screens/Settings.js
+++ client/app/screens/Settings.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, SectionList, StyleSheet } from 'react-native';
 import Screen from './Screen';
+import TwoLineListItem from '../components/TwoLineListItem';
 
 class Settings extends Screen {
     render() {
         return (
             <View style={styles.container}>
+                <SectionList
+                    sections={[
+                        {
+                            data: [{
+                                key: 1,
+                                title: '계정 정보',
+                                subtitle: '계정 정보를 확인합니다.'
+                            }, {
+                                key: 2,
+                                title: '로그아웃',
+                                subtitle: '현재 로그인한 계정을 로그아웃합니다.'
+                            }],
+                            title: "계정",
+                            renderItem: ({item}) => {
+                                return (
+                                    <TwoLineListItem
+                                        title={item.title}
+                                        subtitle={item.subtitle}
+                                    />
+                                );
+                            }
+                        }
+                    ]}
+                />
             </View>
         )
     }
